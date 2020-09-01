86aa68bdb5bd34d45fc5851a927073765d0f4582
Elias da Rosa <elias@eliasdarosa.com.br>
2020-09-01 16:56:27 -0300

    Added token

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -8,14 +8,14 @@ sessions.post('/', async (request, response) => {
     const { email, password } = request.body;
     const session = new CreateSessionService();
 
-    const { user } = await session.execute({
+    const { user, token } = await session.execute({
       email,
       password,
     });
 
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -1,14 +1,16 @@
 import { getRepository } from 'typeorm';
-// import { hash } from 'bcryptjs';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
   email: string;
   password: string;
 }
+
 interface Response {
   user: User;
+  token: string;
 }
 
 class CreateSessionService {
@@ -29,7 +31,12 @@ class CreateSessionService {
       throw Error('Incorrent e-mail/password combination.');
     }
 
-    return { user };
+    const token = sign({}, 'a1d7896a3eed8933878a053bf5e9a7e9', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
